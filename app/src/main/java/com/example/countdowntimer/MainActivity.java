aa128da093679d34a9821fdfa5a0b1faa4d8a2cb
JaanbaazAkhtar <jaanbaazakhtar786@gmail.com>
2021-04-30 02:31:40 +0530

    CountDown Timer Completed

--- app/src/main/java/com/example/countdowntimer/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private static final long START_TIME_IN_MILLIS = 600000;
 
@@ -44,11 +46,49 @@ public class MainActivity extends AppCompatActivity {
                 resetTimer();
             }
         });
+        updateCountDownText();
     }
 
     private void startTimer()
     {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
 
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                startPausebtn.setText("Start");
+                startPausebtn.setVisibility(View.INVISIBLE);
+                resetbtn.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        mTimerRunning = true;
+        startPausebtn.setText("Pause");
+        resetbtn.setVisibility(View.INVISIBLE);
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int) mTimeLeftInMillis/1000/60;
+        int seconds = (int) (mTimeLeftInMillis/1000)%60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes,seconds);
+        CountDownText.setText(timeLeftFormatted);
+    }
+
+    private void pauseTimer(){
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        startPausebtn.setText("Start");
+        resetbtn.setVisibility(View.VISIBLE);
+    }
+    private void resetTimer(){
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        resetbtn.setVisibility(View.INVISIBLE);
+        startPausebtn.setVisibility(View.VISIBLE);
     }
 
 }
